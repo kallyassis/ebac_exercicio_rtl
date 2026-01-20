25b1e700959f8cac5de3ee1bfa46ef513d21e05e
kalinka <kallysantosk@gmail.com>
2026-01-20 14:58:46 -0300

    criando teste no postComments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test('Deve inserir 2 comentarios', () => {
+    render(<PostComment />);
+
+    const textarea = screen.getByTestId("campo-ip");
+    const button = screen.getByRole('button', {name: /Comentar/i});
+
+    fireEvent.change(textarea, {
+      target: {value: 'primeiro comentario'},
     });
+    fireEvent.click(button);
+    expect(screen.getByText('primeiro comentario')).toBeInTheDocument();
+
+    fireEvent.change(textarea, {
+      target: {value: 'segundo comentario'},
+    });
+    fireEvent.click(button);
+    expect(screen.getByText('segundo comentario')).toBeInTheDocument();
+  })
 });
